bd1ce355d03a9fecc2ce4f9e1d81e69c17b46890
Jésus Baro <118184872+JesusBaro@users.noreply.github.com>
2023-01-29 19:45:59 -0300

    inserindo arquivos

---
+++ script.js
@@ -0,0 +1,21 @@
+//1 - função que exiba uma mensagem no console
+let msg = `Olha sou uma mensagem e espero aparecer no console.log `
+function exibirmsg() {
+    console.log(msg)
+}
+exibirmsg();
+
+
+//2 - função que receba o seu nome como (parâmetro) e exiba no console
+
+
+//3 - função que receba nome, idade, e um estilo musical preferido (parâmetros) e exiba no console
+
+
+//4 - função que receba um filme, uma música (parâmetros) e exiba no console
+
+
+//5 - função que retorne o triplo do número recebido no parâmetro da função
+
+
+//6 - função que  verifique se uma  variável é true ou false
